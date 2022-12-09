b1b6d1fd591650c6a72d5132a3173607fee22c81
Nguyen Duc Nam <namworkmc@gmail.com>
2022-12-09 12:06:42 +0700

    Init front repo

---
+++ vite.config.ts
@@ -0,0 +1,44 @@
+import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default ({mode}: any) => {
+  // Load app-level env vars to node-level env vars.
+  return defineConfig({
+    plugins: [react()],
+    build: {
+      commonjsOptions: {
+        esmExternals: true,
+      },
+    },
+    server: {
+      port: 3000,
+    },
+    define: {
+      global: 'window',
+      process: {
+        env: {...loadEnv(mode, process.cwd(), '')},
+      },
+    },
+    resolve: {
+      alias: {
+        '~animate.css': 'animate.css',
+        '~bootstrap': 'bootstrap',
+        '~bootstrap-icons': 'bootstrap-icons',
+        '~socicon': 'socicon',
+        '~@fortawesome': '@fortawesome',
+        '~line-awesome': 'line-awesome',
+        '~prism-themes': 'prism-themes',
+
+        // Path resolver
+        '@': path.resolve(__dirname, './src'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@models': path.resolve(__dirname, './src/models'),
+        '@services': path.resolve(__dirname, './src/services'),
+        '@_metronic': path.resolve(__dirname, './src/_metronic'),
+      },
+    },
+  });
+};
