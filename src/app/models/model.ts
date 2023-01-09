6f8d80cc8870b2c2a061b9d8ebb004bdd4f39046
Nguyen Duc Nam <namworkmc@gmail.com>
2023-01-09 21:42:32 +0700

    [PREM-43] Remove ignoring model.ts

---
+++ src/app/models/model.ts
@@ -0,0 +1,237 @@
+/* tslint:disable */
+/* eslint-disable */
+// Generated using typescript-generator version 3.0.1157 on 2023-01-09 19:58:43.
+
+export interface CreateLoanReminderDto {
+    creditorCreditCardNumber: string;
+    debtorCreditCardNumber: string;
+    debtorName: string;
+    transferAmount: number;
+    loanRemark: string;
+    debtorPhone: string;
+}
+
+export interface CreditCardDto extends PremiereAbstractEntityDto {
+    balance: number;
+    openDay: Date;
+    cardNumber: string;
+    userId: number;
+}
+
+export interface DepositMoneyExternalRequestDto {
+    senderCreditCardNumber: string;
+    receiverCreditCardNumber: string;
+    amount: number;
+    senderBankName: string;
+    rsaToken: string;
+}
+
+export interface DepositMoneyRequestDto {
+    username: string;
+    creditCardNumber: string;
+    amount: number;
+}
+
+export interface EmployeeStatusDto {
+    username: string;
+    enabled: boolean;
+}
+
+export interface ErrorDto {
+    message: string;
+    i18nPlaceHolder: string;
+}
+
+export interface FullInfoUserDto extends PremiereAbstractEntityDto {
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    gender: string;
+    panNumber: string;
+    address: string;
+    password?: string;
+    role: string;
+    enabled: boolean;
+}
+
+export interface LoanReminderDto {
+    id: number;
+    version: number;
+    transferAmount: number;
+    status: LoanStatus;
+    time: Date;
+    updatedTime: Date;
+    loanRemark: string;
+    senderCreditCardId: number;
+    receiverCreditCardId: number;
+    senderCreditCardNumber: string;
+    senderName: string;
+    senderId: number;
+    receiverCreditCardNumber: string;
+    receiverName: string;
+    receiverId: number;
+    cancelReason: string;
+}
+
+export interface LoanReminderMessageDto {
+    senderName: string;
+    senderId: number;
+    receiverName: string;
+    receiverId: number;
+    message: string;
+    destination: string;
+}
+
+export interface MetaDataDto {
+    pagination: PaginationDto;
+}
+
+export interface OTPDto {
+    otp: string;
+    email: string;
+    createdAt: Date;
+}
+
+export interface PaginationDto {
+    currPage: number;
+    sizePerPage: number;
+    currPageTotalElements: number;
+    totalElements: number;
+    totalPages: number;
+    first: boolean;
+    last: boolean;
+}
+
+export interface PasswordDto {
+    username: string;
+    email: string;
+    currentPassword: string;
+    newPassword: string;
+}
+
+export interface PremiereAbstractEntityDto {
+    id?: number;
+    version: number;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface PremierePaginationResponseDto<T> {
+    payload: T[];
+    meta: MetaDataDto;
+}
+
+export interface ReceiverDto {
+    id: number;
+    cardNumber: string;
+    nickname: string;
+    fullName: string;
+    userId: number;
+    bankName: string;
+}
+
+export interface TransactionCriteriaDto {
+    page?: number;
+    size?: number;
+    transactionType?: TransactionType;
+    asc?: boolean;
+}
+
+export interface TransactionDto {
+    id: number;
+    version: number;
+    createdAt: Date;
+    updatedAt: Date;
+    amount: number;
+    type: TransactionType;
+    time: Date;
+    transactionRemark: string;
+    totalBalance: number;
+    senderCreditCardNumber: string;
+    receiverCreditCardNumber: string;
+    fee: number;
+    status: TransactionStatus;
+    senderBankId: number;
+    receiverBankId: number;
+    selfPaymentFee: boolean;
+}
+
+export interface TransactionRequestDto {
+    senderCardNumber: string;
+    receiverCardNumber: string;
+    amount: string;
+    type: string;
+    isInternal: boolean;
+    isSelfPaymentFee: boolean;
+    remark: string;
+    senderBankName: string;
+    receiverBankName: string;
+    otp: string;
+}
+
+export interface TransferMoneyRequestDto {
+    requestID: number;
+    otp: string;
+}
+
+export interface UserDto {
+    id: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+    role: PremiereRole;
+}
+
+export interface WebSocketResponseDto<T> {
+    action: WebSocketAction;
+    message: string;
+    data: T;
+}
+
+export const enum AlgorithmType {
+    RSA = "RSA",
+}
+
+export const enum CardStatus {
+    AVAILABLE = "AVAILABLE",
+    DISABLED = "DISABLED",
+}
+
+export const enum Gender {
+    MALE = "MALE",
+    FEMALE = "FEMALE",
+    OTHER = "OTHER",
+}
+
+export const enum LoanStatus {
+    APPROVED = "APPROVED",
+    PENDING = "PENDING",
+    REJECTED = "REJECTED",
+    CANCELLED = "CANCELLED",
+    PAID = "PAID",
+}
+
+export const enum PremiereRole {
+    CUSTOMER = "CUSTOMER",
+    EMPLOYEE = "EMPLOYEE",
+    PREMIERE_ADMIN = "PREMIERE_ADMIN",
+}
+
+export const enum TransactionStatus {
+    CHECKING = "CHECKING",
+    COMPLETED = "COMPLETED",
+    FAILED = "FAILED",
+}
+
+export const enum TransactionType {
+    MONEY_TRANSFER = "MONEY_TRANSFER",
+    LOAN = "LOAN",
+}
+
+export const enum WebSocketAction {
+    DEPOSIT_MONEY = "DEPOSIT_MONEY",
+}
